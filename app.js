105fda36d029fb621f91a35a859d58e7fcf61755
dujaejeong <gbtwld@gmail.com>
2020-09-09 03:24:41 +0900

    Add controllers and views

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter";
-import routes from "./routes";
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import globalRouter from './routers/globalRouter'
+import userRouter from './routers/userRouter'
+import videoRouter from './routers/videoRouter'
+import routes from './routes'
 
-const app = express();
+const app = express()
 
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan("dev"));
+app.set('view engine', 'pug')
 
-app.use(routes.home, globalRouter);
-app.use(routes.users, userRouter);
-app.use(routes.videos, videoRouter);
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(morgan('dev'))
 
-export default app;
+app.use(routes.home, globalRouter)
+app.use(routes.users, userRouter)
+app.use(routes.videos, videoRouter)
+
+export default app
