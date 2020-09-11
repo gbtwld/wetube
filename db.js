106fc54f1d49adc6bcd15c1cd6642e93f83f1b48
gbtwld <gbtwld@gmail.com>
2020-09-11 22:07:10 +0900

    Created fake db

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome",
+        description: "This is something I love",
+        view: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nico",
+            email: "nico@las.com",
+        },
+    },
+    {
+        id: 1241231,
+        title: "Video good",
+        description: "This is something I love",
+        view: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nico",
+            email: "nico@las.com",
+        },
+    },
+    {
+        id: 325234,
+        title: "Video super",
+        description: "This is something I love",
+        view: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nico",
+            email: "nico@las.com",
+        },
+    },
+    {
+        id: 523412,
+        title: "Video wow",
+        description: "This is something I love",
+        view: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nico",
+            email: "nico@las.com",
+        },
+    },
+];
