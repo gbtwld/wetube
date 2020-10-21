5f9cdfdc7a04f32c81caee180501dca9916599b5
gbtwld <gbtwld@gmail.com>
2020-10-21 22:49:24 +0900

    Facebook login and user details

--- passport.js
+++ passport.js
@@ -1,30 +1,44 @@
 import passport from "passport";
 import GithubStrategy from "passport-github";
-import { githubLoginCallback } from "./controllers/userController";
+import FacebookStrategy from "passport-facebook";
+import { facebookLoginCallback, githubLoginCallback } from "./controllers/userController";
 import User from "./models/User";
 import routes from "./routes";
 
 passport.use(User.createStrategy());
 
 passport.use(
-  new GithubStrategy(
-    {
-      clientID: process.env.GH_ID,
-      clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`,
-      scope: "user:email",
-    },
-    githubLoginCallback
-  )
+    new GithubStrategy(
+        {
+            clientID: process.env.GH_ID,
+            clientSecret: process.env.GH_SECRET,
+            callbackURL: `http://localhost:4000${routes.githubCallback}`,
+            scope: "user:email",
+        },
+        githubLoginCallback
+    )
+);
+
+passport.use(
+    new FacebookStrategy(
+        {
+            clientID: process.env.FB_ID,
+            clientSecret: process.env.FB_SECRET,
+            callbackURL: `https://shaggy-yak-42.loca.lt${routes.facebookCallback}`,
+            profileFields: ["id", "displayName", "photos", "email"],
+            scope: ["public_profile", "email"],
+        },
+        facebookLoginCallback
+    )
 );
 
 passport.serializeUser(
-  User.serializeUser(function (user, done) {
-    done(null, user);
-  })
+    User.serializeUser(function (user, done) {
+        done(null, user);
+    })
 );
 passport.deserializeUser(
-  User.deserializeUser(function (user, done) {
-    done(null, user);
-  })
+    User.deserializeUser(function (user, done) {
+        done(null, user);
+    })
 );
