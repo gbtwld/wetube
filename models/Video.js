b7311c83ea199d2feeb933d82a4e109772ab386d
dujaejeong <gbtwld@gmail.com>
2020-10-24 00:41:16 +0900

    Video player and views and comments clear!!

--- models/Video.js
+++ models/Video.js
@@ -1,35 +1,34 @@
 import mongoose from "mongoose";
 
 const VideoSchema = new mongoose.Schema({
-    fileUrl: {
-        type: String,
-        required: "File URL is required.",
+  fileUrl: {
+    type: String,
+    required: "File URL is required",
+  },
+  title: {
+    type: String,
+    required: "Tilte is required",
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
     },
-    title: {
-        type: String,
-        required: "Title is required.",
-    },
-    description: String,
-    view: {
-        type: Number,
-        default: 0,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment",
-        },
-    ],
-    creator: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
+  ],
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const model = mongoose.model("Video", VideoSchema);
-
 export default model;
